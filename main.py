38b28a9d44f165c2704c3637d9d520af65128db6
alessio-bruzzese <alessio.bruzzese@edu.esiee.fr>
2024-11-07 09:39:22 +0000

    fonction pour vérifier un nombre premier

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
+"""module qui vérifie les nombres premiers"""
+
 from math import sqrt
 
 #### Fonction secondaire
 
-
 def isprime(p):
-
+    """fonction vérifiant que le nombre passé en paramètre est premier"""
     # votre code ici
+    prime = True
 
-    pass
+    if p==1 :
+        return False
+
+    for i in range (2, int(sqrt(p))+1):
+        if p%i==0:
+            prime = False
+    return prime
 
 #### Fonction principale
 
-
 def main():
-
+    """permet le test sur 5 nombres"""
     # vos appels à la fonction secondaire ici
 
-    for n in range(100):
-        if isprime(n):
-            print(n, end=", ")
-
-    print()
+    print(isprime(1))
+    print(isprime(2))
+    print(isprime(67))
+    print(isprime(45))
+    print(isprime(60))
 
 
 if __name__ == "__main__":
